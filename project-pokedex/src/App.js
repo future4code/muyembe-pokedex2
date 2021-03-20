74d06930d3a9f4f4f95e909b5335b8891ab3cbf0
edgardfn <edgardfinotti@yahoo.com.br>
2021-03-19 21:30:47 -0300

    Criado componente Header.

--- project-pokedex/src/App.js
+++ project-pokedex/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import React from 'react';
+import Header from './components/Header/Header'
 
-const  App = () => {
+const App = () => {
+
+
   return (
     <div>
-
+      <Header />
     </div>
   );
 }
