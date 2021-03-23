9de3e016117e8091fcf5188d8fd6d096c013d973
edgardfn <edgardfinotti@yahoo.com.br>
2021-03-22 22:04:05 -0300

    Realizado limpeza de codigos de testes comentados e console logs.

--- project-pokedex/src/components/PokemonCard/PokemonCard.js
+++ project-pokedex/src/components/PokemonCard/PokemonCard.js
@@ -6,66 +6,13 @@ import { ButtonAddPokedex, ButtonDetails, ButtonsContainer, ImagePoke, ImagePoke
 
 
 const PokemonCard = ({urlPokemon}) => {
-    console.log("urlPokemon", urlPokemon)
     const dataInfoPoke = useRequestData([], urlPokemon);
-    // const [urlImagePoke, setUrlImagePoke] = useState("")
-
-    // useEffect(() => {
-    //     const likImagemPokemon = dataInfoPoke.sprites.front_default
-    //     setUrlImagePoke(likImagemPokemon)
-    // }, [dataInfoPoke])
     
-    // const [data, setData] = useState({})
-    
-    // useEffect(() => {
-    //     axios
-    //       .get(`${urlPokemon}`)
-    //       .then((response) => {
-    //         console.log("response", response)
-    //         setData(response.data);
-    //       })
-    //       .catch((error) => {
-    //         console.log(error);
-    //         alert("Ocorreu um erro, tente novamente");
-    //       });
-    // }, [urlPokemon]);
 
-    // useEffect(() => {
-    //     getInfoPokemon()
-    // }, []);
-    
-    // console.log("dataInfoPoke", dataInfoPoke)
-    // console.log("data", data)
-
-    // const getInfoPokemon = async () => {
-    //     try{
-    //         const response = await axios.get(`${urlPokemon}`)
-    //         setData(response.data)
-    //     }catch(error) {
-    //         console.log(error.response)
-    //     }
-    // }
-
-    // const componentInfoPoke = 
-    // dataInfoPoke.sprites &&
-    //     <PokeCardContainer>
-    //         <ImagePokeContainer>
-    //             <ImagePoke src={dataInfoPoke.sprites.front_default} alt={"imagem"} />
-    //         </ImagePokeContainer>
-    //         <ButtonsContainer>
-    //             <ButtonAddPokedex>Adicionar a Pokedex</ButtonAddPokedex>
-    //             <ButtonDetails>Ver detalhes</ButtonDetails>
-    //         </ButtonsContainer>
-    //     </PokeCardContainer>
-
-   
-
-
-    console.log("dataInfoPoke", dataInfoPoke)
     return (
         <PokeCardContainer>
             <ImagePokeContainer>
-                <ImagePoke alt={"imagem"} src={(dataInfoPoke.sprites && dataInfoPoke.sprites.front_default)  ? dataInfoPoke.sprites.front_default : "sem imagem"}  />
+                <ImagePoke alt={"imagem"} src={(dataInfoPoke.sprites && dataInfoPoke.sprites.front_default)  && dataInfoPoke.sprites.front_default }  />
             </ImagePokeContainer>
             <ButtonsContainer>
                 <ButtonAddPokedex>Adicionar a Pokedex</ButtonAddPokedex>
